a74210296c6eb7a09ce0557379b32a98dd9c492f
Shubhangi Gupta <shubhangi.gupta.eee11@iitbhu.ac.in>
2015-06-30 22:44:41 +0000

    update fw and user example

--- firmware/rproc-pru1-fw.c
+++ firmware/rproc-pru1-fw.c
@@ -43,6 +43,7 @@ volatile register uint32_t __R31;
 /* Mapping Constant table register to variable */
 volatile pruCfg CT_CFG __attribute__((cregister("PRU_CFG", near), peripheral));
 volatile far pruIntc CT_INTC __attribute__((cregister("PRU_INTC", far), peripheral));
+volatile far uint32_t CT_DRAM __attribute__((cregister("PRU_DMEM_0_1", near), peripheral));
 
 /* Defines */
 #define PRU1
@@ -54,6 +55,8 @@ void main(){
 	/* Configure GPI and GPO as Mode 0 (Direct Connect) */
 	CT_CFG.GPCFG0 = 0x0000;
 
+	uint32_t *dram = (uint32_t *) &CT_DRAM;
+
 	/* Clear GPO pins */
 	__R30 &= 0x00000000;
 	/* Spin in loop until interrupt on HOST 1 is detected */
@@ -61,8 +64,13 @@ void main(){
 		/* Wait for sysevent 16 from ARM which mapped to HOST1 */
 		if (__R31 & HOST1_MASK){
 			TOGGLE_BLUE;
+
+			/* Add values at first two DRAM addresses and store result in 3rd address */
+			dram[2] = dram[0] + dram[1];
+			
 			/* Interrupt ARM using sys evt 32 */
 			__R31 |= 0x00000021;
+			
 			/* Clear interrupt event */
 			CT_INTC.SICR = 16;
 			__delay_cycles(5);
--- userspace/examples/write.c
+++ userspace/examples/write.c
@@ -10,36 +10,34 @@
 int main(void)
 {
 	int i=0;
-	int data[6]={102,-4,2,457,4325,757};
-	int read[6]={0};
+	int data[2]={40,33};
+	int read[1]={0};
 	int err;
 
 	err = pruss_boot("/lib/rproc-pru1-fw",PRU1);
 	if(err)
 		return err;
 
-	err = pruss_write(PRU_DRAM0,0,data,sizeof(data));
+	err = pruss_write(PRU_DRAM1,0,data,sizeof(data));
 	if(err){
 		perror("Write error");
 		return err;
 	}
-		
-	err = pruss_read(PRU_DRAM0,0,read,sizeof(read));
 	
-	if(err){
-		perror("Read error");
-		return err;
-	}
-
 	err = pruss_interrupt(16);
-
 	if(err){
 		perror("Interrupt error");
 		return err;
 	}
+
+	err = pruss_read(PRU_DRAM1,2,read,sizeof(read));
+	if(err){
+		perror("Read error");
+		return err;
+	}
 	
 	printf("Data read\n");
-	for(i=0;i<6;i++)
+	for(i=0;i<sizeof(read)/sizeof(int);i++)
 		printf("%d ", *(read+i));
 	printf("\n");
 	
